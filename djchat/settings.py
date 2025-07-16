e90427eb59925ce684dc9bf2280b4c749b23eac2
Sandeep Yadav <yadav.sandeep95@nic.in>
2025-07-16 15:56:07 +0530

    added default authentication session

--- djchat/settings.py
+++ djchat/settings.py
@@ -123,6 +123,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "account.Account"
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
 }
 SPECTACULAR_SETTINGS = {
     "TITLE": "CHAT Application APIs",
