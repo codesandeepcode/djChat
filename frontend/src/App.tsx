c01b5b1178e6944ed3c6f7365ed6af3b51d49ab2
Sandeep Yadav <yadav.sandeep95@nic.in>
2025-07-21 14:35:53 +0530

    added react-router-dom and home path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
-function App() {
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
 
-  return (
-    <>
-    </>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+    <Route path="/" element = {< Home />} />
+      </Route>
   )
-}
+);
+
+const App: React.FC = () => {
+  return <RouterProvider router={ router } />;
+};
 
 export default App
